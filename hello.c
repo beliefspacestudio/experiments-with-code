10f6fc399f03d72545225ce2dbaf04d9ad850f24
Stu Welsh <studio@beliefspace.co.uk>
2016-09-10 15:07:44 +0100

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,3 +1,14 @@
+/*
+There are several ways to compile this C program, based on which compilers you'e got installed.
+By far the easiest way to do it on a Mac is open Terminal:
+Navigate to your working directory cd /Path/To/Directory 
+$ make hello
+To run the code, type
+$ ./hello #or simply /Path/To/File
+// This works on all Unix machines, including the Raspi...
+$ gcc -o hello hello.cpp
+*/
+
 #include <stdio.h>
 
 int main() {
